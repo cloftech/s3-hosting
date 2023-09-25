e8444cb8a2215fa786d31b8534df1da9abd0b587
hassan-abid-1 <127089221+hassan-abid-1@users.noreply.github.com>
2023-09-25 14:07:49 +0500

    remove var (bucket_name)

--- infrabase/main.tf
+++ infrabase/main.tf
@@ -2,5 +2,5 @@ module "s3" {
   source           = "./modules/s3"
   project_name     = var.project_name
   environment_name = var.environment_name
-  bucket_name      = var.bucket_name
+  # bucket_name      = var.bucket_name
 }
