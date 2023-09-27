580907bc92de985bec6332d6ec6164782b855cf3
hassan-abid-1 <127089221+hassan-abid-1@users.noreply.github.com>
2023-09-27 07:46:12 +0500

    few changes in output files

--- infrabase/modules/s3/output.tf
+++ infrabase/modules/s3/output.tf
@@ -2,3 +2,8 @@ output "website_url" {
   description = "URL of the website"
   value       = aws_s3_bucket_website_configuration.static_hosting_bucket_website_configuration.website_endpoint
 }
+
+output "bucket" {
+  description = "Bucket name"
+  value       = aws_s3_bucket.static_hosting.bucket
+}
--- infrabase/output.tf
+++ infrabase/output.tf
@@ -2,3 +2,8 @@ output "website_url" {
   description = "URL of the website"
   value       = module.s3.website_url
 }
+
+output "name" {
+  description = "Bucket name"
+  value       = module.s3.bucket
+}
