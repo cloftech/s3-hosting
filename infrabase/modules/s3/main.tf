396980d6d13a6d28c09d89d1b2e3930bed20d149
hassan-abid-1 <127089221+hassan-abid-1@users.noreply.github.com>
2023-09-27 08:58:29 +0500

    Final commit

--- infrabase/modules/s3/main.tf
+++ infrabase/modules/s3/main.tf
@@ -53,23 +53,23 @@ resource "aws_s3_bucket_website_configuration" "static_hosting_bucket_website_co
   }
 }
 
-#Putting objects in the bucket.
-resource "aws_s3_object" "hosting_bucket_files" {
-  bucket = aws_s3_bucket.static_hosting.id
+# #Putting objects in the bucket.
+# resource "aws_s3_object" "hosting_bucket_files" {
+#   bucket = aws_s3_bucket.static_hosting.id
 
-  for_each = module.template_files.files
+#   for_each = module.template_files.files
 
-  key          = each.key
-  content_type = each.value.content_type
+#   key          = each.key
+#   content_type = each.value.content_type
 
-  source  = each.value.source_path
-  content = each.value.content
+#   source  = each.value.source_path
+#   content = each.value.content
 
-  etag = each.value.digests.md5
-}
+#   etag = each.value.digests.md5
+# }
 
-module "template_files" {
-  source = "hashicorp/dir/template"
+# module "template_files" {
+#   source = "hashicorp/dir/template"
 
-  base_dir = "${path.module}/../../../codebase/my-app/build"
-}
+#   base_dir = "${path.module}/../../../codebase/my-app/build"
+# }
